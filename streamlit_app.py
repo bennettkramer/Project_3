4f3a1bb475cbb7772dc8d90bd01ccb9fa8f43b6e
Ryan Anderson <98444459+Ryanderson94@users.noreply.github.com>
2022-08-18 20:17:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,7 @@ import pandas as pd
 import requests
 import streamlit as st
 
-# Search Location by City Name
-location_search_url = "https://booking-com.p.rapidapi.com/v1/hotels/locations"
-hotel_search_url = "https://booking-com.p.rapidapi.com/v1/hotels/search"
-headers = {
-	"X-RapidAPI-Key": "fb5b528af4mshb57de5533dbee6fp1285bfjsn290be06d9c2a",
-	"X-RapidAPI-Host": "booking-com.p.rapidapi.com"
-}
+from functions import get_location, get_hotels
 
 st.title("**Welcome to Block A Room**")
 st.subheader("*Creation of Team 1*")
@@ -33,43 +27,19 @@ room_number = str(room_number)
 checkin_date = str(checkin_date)
 checkout_date = str(checkout_date)
 
-# Create Location Query Dictionary
-location_querystring = {
-    "locale":"en-gb",
-    "name":city
-}
 
 # Search Locations
-if st.button('Set Location'):
-    location_response = requests.request("GET", url=location_search_url, headers=headers, params=location_querystring)
+if st.checkbox('Set Location'):
+    destination_id = get_location(city)
 
-    # Obtain Destination Id
-    location_data = location_response.json()
-    destination_id = location_data[0]['dest_id']
-
-    # Create Hotel Query Dictionary
-    hotel_search_querystring = {
-        "checkin_date":checkin_date,
-        "checkout_date":checkout_date,
-        "units":"metric",
-        "dest_id":destination_id,
-        "dest_type":"city",
-        "locale":"en-gb",
-        "adults_number":adults_number,
-        "order_by":"popularity",
-        "filter_by_currency":"USD",
-        "room_number":room_number,
-        "include_adjacency":"true"
-    }
-
-st.text(destination_id)
 
 # Search hotels
-if st.button('Search'):
-    hotel_response = requests.request("GET", url = hotel_search_url, headers=headers, params=hotel_search_querystring)
+if st.checkbox('Search'):
+    hotel_response = get_hotels(destination_id, checkin_date, checkout_date, adults_number, room_number)
 
 
-if st.button('Display Dataframe'):
+if st.checkbox('Display Dataframe'):
+    st.text('List Ordered by Popularity')
     hotel_data = hotel_response.json()
     hotel_list = []
     price_list = []
@@ -86,4 +56,4 @@ if st.button('Display Dataframe'):
     hotel_price_df.columns = ['Hotel Name', 'Price']
     hotel_price_df.set_index('Hotel Name')
 
-st.dataframe(hotel_price_df)
+st.dataframe(hotel_price_df, 1200)
